07e72a276c564358d2c7ab6d0c110846c35653ff
John Ricaurte <montblackjj@gmail.com>
2014-10-21 09:02:01 -0500

    Primera Clase

---
+++ src/main/java/co/com/bogotajvm/manejoexcepciones/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.bogotajvm.manejoexcepciones;
+
+/**
+ *
+ * @author John Ricaurte<john.ricaurte.ext@claro.com.co>
+ */
+public class Main {
+
+}
